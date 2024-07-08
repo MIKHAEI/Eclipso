2f963147ae0043216d7118664696e0f3e74d66b3
MIKHAEI <harshadodamani4242@gmail.com>
2024-07-08 13:43:20 +0530

    Add initial project files

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "odrsdb");
+$con=mysqli_connect("sql213.infinityfree.com", "if0_36860279", "", "if0_36860279_odrsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
